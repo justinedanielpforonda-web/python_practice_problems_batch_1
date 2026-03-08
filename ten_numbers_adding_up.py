573cb33b2479d4b33e0547da84d788ad2ef336a4
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-03-08 16:26:39 +0800

    problem_7_input_addition

---
+++ ten_numbers_adding_up.py
@@ -0,0 +1,8 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+sum = 0
+for i in range(10):
+    numss = int(input(" number please: "))
+    sum = sum + numss
+
+print (sum)
