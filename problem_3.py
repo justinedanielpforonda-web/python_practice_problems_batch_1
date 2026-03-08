d7945b798998674a8ec29a381b2eacf07c51a6d3
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-03-08 16:05:40 +0800

    addition

---
+++ problem_3.py
@@ -0,0 +1,5 @@
+# # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+num_1 = float(input("Input the number: "))
+num_2 = float(input("Input the second numer:"))
+print(num_1 + num_2)
