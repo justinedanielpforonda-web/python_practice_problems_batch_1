f9459b8bf96b0fde4ba7ae1e31b412fb300a0944
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-03-08 16:12:29 +0800

    dividing numbers

---
+++ division_with_decimal.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+
+num_1 = float(input("Input the number: "))
+num_2 = float(input("Input the second numer:"))
+print(num_1 / num_2)
+
