395554031b0bd350f2d1a377d5d86bde3f6cbc73
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-03-08 15:56:55 +0800

    first problem

---
+++ comparing_numbers_problem_1.py
@@ -0,0 +1,11 @@
+# # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+num_1 = float(input("Input the number: "))
+num_2 = float(input("Input the second numer:"))
+
+if num_1 > num_2:
+    print(f"{num_1} is higher")
+else:
+    print(f"{num_2} is higher")
+
+
